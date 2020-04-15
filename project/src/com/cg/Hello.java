066b106a2c291703d214197fda54b01707cd704c
rishitha rao <rishitha rao@DESKTOP-QCRNR46>
2020-04-15 12:08:18 +0530

    Initial commit

---
+++ project/src/com/cg/Hello.java
@@ -0,0 +1,6 @@
+package com.cg;
+
+public interface Hello {
+String sayHello();
+
+}
---
+++ project/src/com/cg/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.cg;
+/**
+ * My Hello World class
+ * @author rishitha rao
+ * @version 1.0
+ *
+ */
+public class HelloWorld implements Hello 
+{
+@Override
+public String sayHello()
+{
+	return "Hello world";
+}
+}
